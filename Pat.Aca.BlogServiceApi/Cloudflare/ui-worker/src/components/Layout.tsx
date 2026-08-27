7b567aa116e208d2a4cba1106cbaa10812001c0d
P47K0 <pkoorevaar@hotmail.com>
2026-08-27 04:10:16 +0200

    Changed fav icon

--- Pat.Aca.BlogServiceApi/Cloudflare/ui-worker/src/components/Layout.tsx
+++ Pat.Aca.BlogServiceApi/Cloudflare/ui-worker/src/components/Layout.tsx
@@ -30,14 +30,8 @@ export const Layout: FC<PropsWithChildren<{ title: string }>> = ({ title, childr
             href="https://www.koorevaar.com"
             class="flex items-center gap-2 rounded-2xl border border-gray-300 bg-white px-5 py-2.5 font-medium text-gray-700 shadow-sm transition hover:bg-gray-50 hover:text-blue-600"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1v-5m10-10l2 2m-2-2v10a1 1 0 01-1 1v-5m-6 0a1 1 0 001-1v5"
-              />
-            </svg>
+            {/*<!--Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->*/}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M224 24c0-13.3 10.7-24 24-24 145.8 0 264 118.2 264 264 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-119.3-96.7-216-216-216-13.3 0-24-10.7-24-24zM80 96c26.5 0 48 21.5 48 48l0 224c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48c-8.8 0-16-7.2-16-16l0-64c0-8.8 7.2-16 16-16 79.5 0 144 64.5 144 144S255.5 512 176 512 32 447.5 32 368l0-224c0-26.5 21.5-48 48-48zm168 0c92.8 0 168 75.2 168 168 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-66.3-53.7-120-120-120-13.3 0-24-10.7-24-24s10.7-24 24-24z" /></svg>
             Home
           </a>
         </div>
